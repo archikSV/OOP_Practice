2dd9430a6ae8925e95e619936553022ef6af5067
SalchukArtem <archik.sv@gmail.com>
2023-10-30 20:58:01 +0200

    Added task 3 to main.py

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-print("Task X:\n")
+print("Task 3:\n")
 
 """
-
+Створіть клас Book з атрибутами title (назва
+книги), author (автор) та genre (жанр). Додайте метод
+display_info, який виведе інформацію про книгу у
+вигляді "Назва: {title}, Автор: {author}, Жанр: {genre}".
 """
 
 # classes
+class Book:
+    def __init__(self, title, author, genre):
+        self.title = title
+        self.author = author
+        self.genre = genre
 
+    def display_info(self):
+        print(f"Title: {self.title} \nАuthor: {self.author} \nGenre: {self.genre}")
 
 # objects
-
+book1 = Book("War and Peace", "Leo Tolstoy", "novel")
 
 # main
+book1.display_info()
