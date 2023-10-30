609f8b2043d3596a3418e9e7750818464df74f97
SalchukArtem <archik.sv@gmail.com>
2023-10-30 20:54:25 +0200

    Added task 2 to main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 print("Task X:\n")
 
 """
-
+Створіть клас Circle з атрибутом radius та методом
+area, який поверне площу кола з вказаним радіусом.
 """
 
 # classes
+class Cirlce:
+    def __init__(self, radius):
+        self.radius = radius
 
+    def area(self):
+        return 3.14 * self.radius ** 2
 
 # objects
-
+cirlce1 = Cirlce(5)
 
 # main
+print(circlce1.area())
