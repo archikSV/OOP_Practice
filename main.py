b32f6d870f2890b8563e2640491f978ca79546c3
SalchukArtem <archik.sv@gmail.com>
2023-10-30 20:53:02 +0200

    Added task 1 to main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-print("Task X:\n")
+print("Task 1:\n")
 
 """
-
+Створіть клас Student з атрибутами name та age.
+Додайте метод print_info, який виведе інформацію про
+студента у на вигляді "Ім'я: {name}, Вік: {age}".
 """
 
 # classes
-
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def print_info(self):
+        print(f"Ім'я: {self.name}, Вік: {self.age}")
 
 # objects
-
+student1 = Student("Jeremy", 21)
 
 # main
+student1.print_info()
